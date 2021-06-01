ee40400e8d9d6e689a546dc3c9aa53509294a000
yuhuilee <qweasd100454@gmail.com>
2021-06-01 16:45:03 +0800

    second version

--- password.py
+++ password.py
@@ -1,9 +1,14 @@
-time = 3
+time = 3 #剩餘次數
+password = 'a123456'
 while time > 0:
 	time = time - 1
-	password = input('請輸入密碼：')
-	if password != 'a123456':
-		print('密碼錯誤！還有', time, '次機會')
-	elif password == 'a123456':
-		print('登入成功')
+	pwd = input('請輸入密碼：')
+	if pwd == password:
+		print('登入成功！')
 		break
+	else:
+		print('登入錯誤！')
+		if time > 0:
+			print('還有', time, '次機會')
+		else:
+			print('沒有機會了！')
